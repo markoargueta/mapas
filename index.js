4d2e03152409adaab8008411212dc08c68b930c8
Marco Argueta <markoargueta@hotmail.com>
2016-11-19 14:23:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-var http=require('http').Server(app);
+var http=require('https').Server(app);
 //Usando socket.io
 var io=require('socket.io')(http);
 
