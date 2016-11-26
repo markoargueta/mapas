e48477b73904bb444f270627010dd540c4809d8d
Marco Argueta <markoargueta@hotmail.com>
2016-11-26 10:27:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ var port=process.env.PORT || 3000; //Esto lo exige heroku
 
 app.get('/',function(req, res){
 	res.sendFile(__dirname + '/index.html');//Ruta página index.html
-});
+});	
 
 http.listen(port,function(){
 	console.log('Escuchando en el puerto: ' + port);
+});
+
+var socketCount = 0; //Contador de conexiones al server
+
+io.on('connection', function(socket){
+	console.log('Usuarios conectado...');
+	socketCount++;
+	//emitiendo mensaje a TODOS los socketsno usuarios conectados
+	io.sockets.emit('usuario conectado', socketCount);
+});//cierra conexión
+
+socket.on('disconnect', function(){
+	socketCount--;//Decremento del contador
+	console.log('Usuario Desconectado');//Mensaje en consola
+	io.sockets.emit('usuario desconectado', )//Mensaje a todos los sockets
 });
